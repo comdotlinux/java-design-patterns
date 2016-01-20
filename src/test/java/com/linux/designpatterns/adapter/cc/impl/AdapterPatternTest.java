c1120d272b173dad4d1821646e9fd20e2dd6924a
Guruprasad Kulkarni <guru@linux.com>
2016-01-20 19:15:49 +0530

    Added Junit for Adapter Design Pattern

---
+++ src/test/java/com/linux/designpatterns/adapter/cc/impl/AdapterPatternTest.java
@@ -0,0 +1,65 @@
+package com.linux.designpatterns.adapter.cc.impl;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.linux.designpatterns.adapter.cc.CcOne;
+import com.linux.designpatterns.adapter.cc.CcTwo;
+
+public class AdapterPatternTest {
+
+	private static final String CARD_EXPIRY_MONTH = "12";
+
+	private static final String CARD_EXPIRY_YEAR = "2020";
+
+	private static final String CC_NUMBER = "1234 5678 9101 1213 1415";
+
+	private static final String CC_OWNER_NAME = "John Doe";
+
+	private static final Short CVV = 123;
+
+	private static final BigDecimal PAYMENT_AMOUNT = BigDecimal.TEN;
+	
+	private CcOne ccOne;
+	
+	private CcOneToCcTwoAdapter underTest;
+	
+	@Before
+	public void setUp(){
+		ccOne = new CcOneImpl();
+		ccOne.setCustomerName(CC_OWNER_NAME);
+		ccOne.setCreditCardNo(CC_NUMBER);
+		ccOne.setCardCVVNo(CVV);
+		ccOne.setCardExpYear(CARD_EXPIRY_YEAR);
+		ccOne.setCardExpMonth(CARD_EXPIRY_MONTH);
+		ccOne.setAmount(PAYMENT_AMOUNT);
+		underTest = null; // reset underTest
+	}
+	
+	
+	@Test
+	public void adapterPatternTest() {
+		//Arrange -- already done in @Before
+
+		//Act
+		underTest = new CcOneToCcTwoAdapter(ccOne);
+		 
+		//Assert
+		 assertThat(underTest.getCardOwnerName(), is(equalTo(CC_OWNER_NAME)));
+		 assertThat(underTest.getTotalAmount(),is(equalTo(PAYMENT_AMOUNT.doubleValue())));
+		 assertThat(underTest.getCardExpMonthDate(), containsString(CARD_EXPIRY_MONTH));
+		 assertThat(underTest.getCardExpMonthDate(), containsString(CARD_EXPIRY_YEAR));
+		 assertThat(underTest.getCardExpMonthDate(), containsString("/"));
+		 assertThat(underTest.getCustCardNo(), is(equalTo(CC_NUMBER)));
+		 assertThat(underTest.getCVVNo(), is(equalTo(CVV.intValue())));
+		
+	}
+
+}
